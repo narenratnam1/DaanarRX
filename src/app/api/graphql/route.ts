2e9bfbe9eb5def7e955645c57abc58434e78abec
Rithik <rithikdoesai@gmail.com>
2025-11-14 20:34:12 -0500

    Integrate GraphQL server into Next.js API routes

---
+++ src/app/api/graphql/route.ts
@@ -0,0 +1,26 @@
+import { ApolloServer } from '@apollo/server';
+import { startServerAndCreateNextHandler } from '@as-integrations/next';
+import { NextRequest } from 'next/server';
+import { typeDefs } from '../../../../../server/graphql/schema';
+import { resolvers } from '../../../../../server/graphql/resolvers';
+import { createGraphQLContext } from '../../../../../server/middleware/auth';
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  formatError: (error) => {
+    console.error('GraphQL Error:', error);
+    return {
+      message: error.message,
+      code: error.extensions?.code || 'INTERNAL_SERVER_ERROR',
+    };
+  },
+});
+
+const handler = startServerAndCreateNextHandler<NextRequest>(server, {
+  context: async (req) => createGraphQLContext({ req }),
+});
+
+export { handler as GET, handler as POST };
+
--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -3,7 +3,7 @@ import { setContext } from '@apollo/client/link/context';
 import { onError } from '@apollo/client/link/error';
 
 const httpLink = createHttpLink({
-  uri: process.env.NEXT_PUBLIC_GRAPHQL_URL || 'http://localhost:4000/graphql',
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_URL || '/api/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
